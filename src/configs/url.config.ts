7608c32d1d219a3265ace9250e52f61f4598684c
ANIRBAN MISHRA <anirbanmishra7005@gmail.com>
2023-10-29 20:18:32 +0530

    updated Base Url

--- src/configs/url.config.ts
+++ src/configs/url.config.ts
@@ -3,3 +3,7 @@
 export const LOGIN_USER_URL = "/api/auth/login";
 export const SIGNUP_USER_URL = "/api/auth/signup";
 export const SIGNOUT_USER_URL = "/api/auth/signout";
+export const REFRESH_TOKEN_URL = "/api/auth/session/refresh";
+
+// BASE URL
+export const BASE_URL = "http://localhost:3000/";
